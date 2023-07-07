0b895d28f093d870c7c4547e9e6ded080d6f4dc0
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2023-07-07 10:55:08 +0200

    signup axios post

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,8 +1,35 @@
 import { Link } from 'react-router-dom';
+import { useRef } from 'react';
+import axiosClient from "../axios-client.js";
+import useStateContext from "../context/ContextProvider.jsx";
 
 const Signup = () => {
+	const nameRef = useRef();
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const passwordConfirmationRef = useRef();
+
+	const { setUser, setToken } = useStateContext();
+
 	const onSubmit = (e) => {
-		e.preventdefault();
+		e.preventDefault();
+		const payload = {
+			name: nameRef.current.value,
+			email: emailRef.current.value,
+			password: passwordRef.current.value,
+			passwordConfirmation: passwordConfirmationRef.current.value
+		}
+		axiosClient.post('/signup', payload)
+			.then(({ data }) => {
+				setToken(data.token);
+				setUser(data.user);
+			})
+			.catch(err => {
+				const response = err.response;
+				if (response && response.status == 422) {
+					console.log(response.data.errors);
+				}
+			})
 	}
 
 	return (
@@ -10,10 +37,10 @@ const Signup = () => {
 			<div className="form">
 				<h1 className="title">Create an account</h1>
 				<form onSubmit={onSubmit}>
-					<input type="text" placeholder="Full Name" />
-					<input type="email" placeholder="Email" />
-					<input type="password" placeholder="Password" />
-					<input type="password" placeholder="Password Conformation" />
+					<input ref={nameRef} type="text" placeholder="Full Name" />
+					<input ref={emailRef} type="email" placeholder="Email" />
+					<input ref={passwordRef} type="password" placeholder="Password" />
+					<input ref={passwordConfirmationRef} type="password" placeholder="Password Conformation" />
 					<button className="btn btn-block">Sign up</button>
 					<p className="message">
 						Already registered? <Link to="/login">Login</Link>
